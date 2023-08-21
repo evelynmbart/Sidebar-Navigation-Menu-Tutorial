123223e5d041cca1bb8439dabc1a91acae661ba8
Evelyn Bart <evelyn.m.bart@gmail.com>
2023-08-21 17:31:50 -0400

    add phosphoricons(6)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Sidebar />
-      <House size={32} />
     </div>
   );
 }
--- src/Components/SidebarData.js
+++ src/Components/SidebarData.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { House } from "phosphor-react";
+import { House, EnvelopeOpen, Fish, Cake, Broom, Dog } from "phosphor-react";
 
 export const SidebarData = [
   {
     title: "Home",
     icon: <House size={32} />,
     link: "/home",
+  },
+  {
+    title: "Mailbox",
+    icon: <EnvelopeOpen size={32} />,
+    link: "/mail",
+  },
+  {
+    title: "Pescetarian",
+    icon: <Fish size={32} />,
+    link: "/fish",
+  },
+  {
+    title: "Baking",
+    icon: <Cake size={32} />,
+    link: "/cake",
+  },
+  {
+    title: "Doggo",
+    icon: <Dog size={32} />,
+    link: "/dog",
+  },
+  {
+    title: "Cleanup",
+    icon: <Broom size={32} />,
+    link: "/broom",
   },
 ];
