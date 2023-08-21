7130e4e7be7a6fc222940d52435c210c9c5b6fd5
Evelyn Bart <evelyn.m.bart@gmail.com>
2023-08-21 16:37:19 -0400

    connect sidebar.js to app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Sidebar } from "./Components/Sidebar";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../App.css";
 
 export function Sidebar() {
   return <div></div>;
